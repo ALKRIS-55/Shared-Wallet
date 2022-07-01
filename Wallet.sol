5db6a95ad1c460ced89f67a95285ace5db37da75
Wasim Shaikh <78263417+ALKRIS-55@users.noreply.github.com>
2022-07-01 21:27:01 +0530

    Create Wallet.sol

---
+++ Wallet.sol
@@ -0,0 +1,23 @@
+//SPDX-License-Identifier: MIT
+contract SharedWallet is Allowance{
+    event Received(address indexed _from, uint _amount);
+    event Sent(address indexed _to, uint _amount);
+
+    function receiveMoney() public payable{
+        emit Received(msg.sender, msg.value);
+
+    }
+    
+    receive() external payable{
+        receiveMoney();
+    }
+
+    function withdrawMoney(address payable _adr, uint _amount) public ownerOrAllowanced(_amount) {
+        require(_amount <= address(this).balance, "Insufficient Fund!");
+        if(!isOwner()){
+            updateAllowance(msg.sender, _amount, _adr);
+        }
+        _adr.transfer(_amount);
+        emit Sent(_adr, _amount);
+    }
+}
